6d04c30d0d72eb711e75e3550969482eaa9abd99
Hanna <hannatarnasen@hotmail.com>
2023-02-27 16:34:38 +0100

    Fixed images and made a testing subset for us

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from gui import TestGui
 import random
 import pickle
 
-img_paths = [str(path) for path in list(Path("Images").glob('*/*.jpg'))]
+img_paths = [str(path) for path in list(Path("Images").glob('*.jpg'))]
 random.shuffle(img_paths)
 c_sz = 4
 
