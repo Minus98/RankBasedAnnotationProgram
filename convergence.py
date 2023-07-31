d15cf09e8864d3b2769330ace4b77f3c53179470
Hannat98 <hannatarnasen@hotmail.com>
2023-07-31 11:18:02 +0200

    Fixed recomputation for rating

--- convergence.py
+++ convergence.py
@@ -1,9 +1,7 @@
-import copy
 import math
 from typing import List
 
-import pandas as pd
-
+import recomputation as recomp
 import saves_handler
 import sorting_algorithms as sa
 
@@ -18,13 +16,15 @@ def get_convergence(save: dict) -> List[float]:
     Returns:
         List[float]: The computed RMS errors.
     """
-    if type(save['sort_alg']) == sa.TrueSkill:
+    if type(save['sort_alg']) in [sa.TrueSkill]:
         if "rmses" not in save or not len(
                 save["rmses"]) == (save["sort_alg"].comp_count-1):
             create_rmses_from_recomputation(save)
+            saves_handler.save_algorithm_pickle(save)
     else:
         if "rmses" not in save:
             save["rmses"] = []
+            saves_handler.save_algorithm_pickle(save)
 
     return save["rmses"]
 
@@ -37,8 +37,9 @@ def create_rmses_from_recomputation(save: dict) -> None:
         save (dict): The dictionary containing the necessary information.
     """
     if type(save['sort_alg']) == sa.TrueSkill:
-        save["rmses"] = recompute_trueskill(save)
-        saves_handler.save_algorithm_pickle(save)
+        _, save["rmses"] = recomp.recompute_trueskill(save)
+    elif type(save['sort_alg']) == sa.HybridTrueSkill:
+        _, save["rmses"] = recomp.recompute_hybridtrueskill(save)
 
 
 def rmses_inference(save: dict, prev_ratings: dict, sort_alg: sa.TrueSkill):
@@ -52,66 +53,9 @@ def rmses_inference(save: dict, prev_ratings: dict, sort_alg: sa.TrueSkill):
         sort_alg (TrueSkill): The TrueSkill sorting algorithm.
     """
     get_convergence(save)
-    save["rmses"].append(calc_rmse(prev_ratings, sort_alg))
-    saves_handler.save_algorithm_pickle(save)
-
-
-def calc_rmse(prev_ratings: dict, sort_alg: sa.TrueSkill) -> float:
-    """
-    Calculates the Root Mean Square Error (RMSE) between previous ratings and current 
-    ratings.
-
-    Args:
-        prev_ratings (dict): The previous ratings dictionary.
-        sort_alg (TrueSkill): The TrueSkill sorting algorithm.
-
-    Returns:
-        float: The computed RMSE.
-    """
     rmse = math.sqrt(
         sum(
             (prev_ratings[key].mu - sort_alg.ratings[key].mu) ** 2
             for key in sort_alg.data) / sort_alg.n)
-    return rmse
-
-
-def recompute_trueskill(save: dict) -> List[float]:
-    """
-    Recomputes TrueSkill algorithm based on the data in 'save' and returns the RMS 
-    errors.
-
-    Args:
-        save (dict): The dictionary containing the necessary information.
-
-    Returns:
-        List[float]: The computed RMS errors.
-    """
-    csv = pd.read_csv(save['path_to_save'] + '.csv')
-    sort_alg = sa.TrueSkill(
-        data=save['sort_alg'].data,
-        comparison_max=save['sort_alg'].comparison_max)
-    rmses = []
-    prev_ratings = copy.deepcopy(sort_alg.ratings)
-
-    for i, i_df in csv.iterrows():
-        res = i_df['result'].replace(
-            "[", "").replace(
-            "]", "").replace(
-            "'", "").split(", ")
-
-        diff_lvls = [
-            sa.DiffLevel(
-                abs([int(s) for s in dl if s.isdigit()][0]))
-            for dl in i_df['diff_levels'].split(", ")]
-
-        if i_df['undone']:
-            pass
-        else:
-            sort_alg.inference(i_df['user'], res, diff_lvls)
-
-            if i > 0:
-                rmses.append(calc_rmse(prev_ratings, sort_alg))
-
-            prev_ratings = copy.deepcopy(sort_alg.ratings)
-
-    return rmses
+    save["rmses"].append(rmse)
+    saves_handler.save_algorithm_pickle(save)
---
+++ recomputation.py
@@ -0,0 +1,85 @@
+import copy
+import math
+from typing import List, Tuple
+
+import pandas as pd
+
+import sorting_algorithms as sa
+
+
+def recompute_trueskill(save: dict) -> Tuple[sa.TrueSkill, List[float]]:
+    """
+    Recomputes the TrueSkill algorithm based on the data in 'save' and returns the 
+    sorting algorithm and appropriate measures.
+
+    Args:
+        save (dict): A dictionary containing the necessary information.
+
+    Returns:
+        Tuple[TrueSkill, List[float]]: A tuple containing the updated TrueSkill 
+        object and a list of computed RMS errors.
+    """
+    csv = pd.read_csv(save['path_to_save'] + '.csv')
+    sort_alg = sa.TrueSkill(
+        data=save['sort_alg'].data,
+        comparison_size=save['sort_alg'].comparison_size,
+        comparison_max=save['sort_alg'].comparison_max)
+    rmses = []
+    prev_ratings = copy.deepcopy(sort_alg.ratings)
+
+    for i, i_df in csv.iterrows():
+        res = i_df['result'].replace(
+            "[", "").replace(
+            "]", "").replace(
+            "'", "").split(", ")
+
+        diff_lvls = [
+            sa.DiffLevel(
+                abs([int(s) for s in dl if s.isdigit()][0]))
+            for dl in i_df['diff_levels'].split(", ")]
+
+        if i_df['undone']:
+            pass
+        else:
+            sort_alg.inference(i_df['user'], res, diff_lvls)
+
+            if i > 0:
+                rmse = math.sqrt(
+                    sum(
+                        (prev_ratings[key].mu - sort_alg.ratings[key].mu) ** 2
+                        for key in sort_alg.data) / sort_alg.n)
+                rmses.append(rmse)
+
+            prev_ratings = copy.deepcopy(sort_alg.ratings)
+
+    return sort_alg, rmses
+
+
+def recompute_ratings(save: dict) -> sa.RatingAlgorithm:
+    """
+    Recomputes the ratings based on the data in 'save' using the rating algorithm and 
+    returns the updated rating algorithm.
+
+    Args:
+        save (dict): A dictionary containing the necessary information.
+
+    Returns:
+        RatingAlgorithm: The updated rating algorithm object.
+    """
+    csv = pd.read_csv(save['path_to_save'] + '.csv')
+    sort_alg = sa.RatingAlgorithm(
+        data=save['sort_alg'].data)
+
+    for _, i_df in csv.iterrows():
+
+        res = i_df['result'].replace(
+            "(", "").replace(
+            ")", "").replace(
+            "'", "").split(", ")
+
+        if i_df['undone']:
+            pass
+        else:
+            sort_alg.inference(i_df['user'], res[0], int(res[1]))
+
+    return sort_alg
